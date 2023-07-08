9a601bcfecae688c90ecf87d9c0f0bb8523cd420
CalledWods <83247090+TheRealOranz@users.noreply.github.com>
2023-07-09 01:56:48 +0200

    trying to add upload ability

---
+++ upload.php
@@ -0,0 +1,7 @@
+<?php
+
+// moves uploaded file to a nice directory
+$targetPath = "uploads/" , basename($_FILES["inpFile"]["name"]);
+move_uploaded_file($_FILES["inpFile"]["tmp_name"], $targetPath);
+
+
